7a5c2321743ee4aadabce553b7e12b86cc2c0bc5
Boka Thierry KOTCHI <thierrykotchi@hotmail.fr>
2018-09-04 17:32:53 +0200

    commit config

--- projetfinal/src/main/java/fr/formation/Principal.java
+++ projetfinal/src/main/java/fr/formation/Principal.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import fr.formation.lamarmite.entities.User;
+import fr.formation.sodibank.entities.User;
 
 public class Principal implements UserDetails {
 
